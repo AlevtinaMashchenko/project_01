02d7c6a0366c58a83217859c5c9de4325689994f
AlevtinaMashchenko <134137775+AlevtinaMashchenko@users.noreply.github.com>
2023-06-23 20:37:43 +0300

    Выполнила д.з. №3

--- lvl 3/task_3.1.py
+++ lvl 3/task_3.1.py
@@ -26,3 +26,40 @@
 #   - использовать готовые классы numpy.array() и pandas.DataFrame() запрещено!
 #   - проявите фантазию :)
 
+
+
+############ Решение ############
+
+import random
+
+class Matrix:
+    def __init__(self, rows = 5, columns = 7):
+        self.rows = rows
+        self.columns = columns
+        self.content = []
+        for j in range(self.rows):
+            k=[]
+            for i in range(self.columns):
+                k.append(random.randint(1, 9))
+            self.content.append(k)
+        
+    def print_Matrix(self):
+        return self.content
+    
+    def edit_Matrix(self, rows1, columns1, value):
+         self.rows1 = rows1
+         self.columns1 = columns1
+         self.value = value
+         self.content[rows1-1][columns1-1] = value
+         print('Таблица с обновленными данными:')
+         return self.content
+
+    def print_columns_and_rows(self):
+        return f'Количество строк равно: {self.rows}. Количество колонок равно: {self.columns}.'
+        
+     
+my_table = Matrix()
+
+print(*my_table.print_Matrix(), sep='\n')
+print(*my_table.edit_Matrix(1,1,"Hello Teaher!"), sep='\n')
+print(my_table.print_columns_and_rows())
